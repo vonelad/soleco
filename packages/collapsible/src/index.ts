3ac0b33d259506a6c6036b5a48da97ffaf1118d0
vonelad <vonelad@gmail.com>
2023-12-24 16:36:15 +0000

    Clean up packages, updates keywords

--- packages/collapsible/src/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-  Primitive,
-  //
-  Root,
-} from "./Primitive";
-export type { PrimitiveProps } from "./Primitive";
--- packages/create-controllable-signal/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { createControllableSignal } from "./createControllableSignal";
